4e7e044e4b695ef6cd2f3d27a4669bba49b0902f
Aidan Weinberg <aidan3445@gmail.com>
2026-02-05 14:28:47 -0600

    delete user

--- src/app/(protected)/(tabs)/profile/account.tsx
+++ src/app/(protected)/(tabs)/profile/account.tsx
@@ -1,4 +1,5 @@
 import { ScrollView, View } from 'react-native';
+import DeleteAccountButton from '~/components/profile/management/delete';
 import AccountSettings from '~/components/profile/management/view';
 
 export default function AccountScreen() {
@@ -11,6 +12,7 @@ export default function AccountScreen() {
         scrollIndicatorInsets={{ top: 10 }}>
         <View className='page justify-start gap-y-4 px-1.5 pt-8 pb-1.5'>
           <AccountSettings locked={false} />
+          <DeleteAccountButton />
         </View>
       </ScrollView>
     </View>
